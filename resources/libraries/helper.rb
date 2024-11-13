006ff0696faf08c3b8ef9f53291d6e33f22ee784
nilsver <nverschaeve@redborder.com>
2024-11-13 09:03:46 +0000

    add script

--- resources/libraries/helper.rb
+++ resources/libraries/helper.rb
@@ -1,4 +1,20 @@
 module Keepalived
   module Helper
+    def find_managers
+      nodes = search(:node, 'roles:manager')
+      manager_hash = {}
+      nodes.each do |n|
+        m_name = n['name']
+        ip_sync = n['ipaddress_sync']
+        ipaddress = n['ipaddress']
+
+        manager_hash[m_name] = {
+          ipaddress_sync: ip_sync,
+          ipaddress: ipaddress
+        }
+
+      end
+      manager_hash
+    end
   end
 end

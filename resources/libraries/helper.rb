15951188d710ce1beb0e4a2cee55cffbe15ae43e
nilsver <nverschaeve@redborder.com>
2024-11-28 09:38:03 +0000

    fix lint

--- resources/libraries/helper.rb
+++ resources/libraries/helper.rb
@@ -3,7 +3,7 @@ module Keepalived
     require 'socket'
 
     def interface_for_ip(ip_address)
-      return nil if ip_address.nil? || ip_address.empty?
+      return if ip_address.nil? || ip_address.empty?
       interfaces = Socket.getifaddrs
       interface = interfaces.find do |ifaddr|
         ifaddr.addr.ipv4? && ifaddr.addr.ip_address == ip_address
